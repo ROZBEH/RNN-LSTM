88d13fed1905894a6cd5a758e86c62061ee7b87f
rozbeh <rouzbeh.asghari@gmail.com>
2017-03-18 10:27:46 -0400

    some changes

--- word2vec_ManyToOne.py
+++
@@ -1,8 +0,0 @@
-
-from gensim.models import word2vec
-import numpy as np
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-model_vec = word2vec.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-print model_vec['dsdsfdf']
-
